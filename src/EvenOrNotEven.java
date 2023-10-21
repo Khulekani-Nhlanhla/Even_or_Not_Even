308d50dc1b3259fd4d1288bcbc96d307107145c0
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-10-21 18:16:43 +0200

    Initial commit

---
+++ src/EvenOrNotEven.java
@@ -0,0 +1,9 @@
+
+public class EvenOrNotEven {
+    public static void main(String[] args) {
+        System.out.println("This program calculates if a number is even or odd!");
+        int randomGeneratedNumber = (int) (Math.random() * (1 - 100));
+        System.out.println("The number generated is ");
+
+    }
+}
