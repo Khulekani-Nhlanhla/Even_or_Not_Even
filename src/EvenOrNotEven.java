fbe9aa105a141421cc206382c4b3492e233733ae
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-10-22 12:10:56 +0200

    I added an if statement to determine if the number generated is an even or odd number.

--- src/EvenOrNotEven.java
+++ src/EvenOrNotEven.java
@@ -1,9 +1,16 @@
 
 public class EvenOrNotEven {
     public static void main(String[] args) {
-        System.out.println("This program calculates if a number is even or odd!");
-        int randomGeneratedNumber = (int) (Math.random() * (1 - 100));
-        System.out.println("The number generated is ");
+        System.out.println("This program calculates if a number is even or odd!The range is between 1 and 100");
+
+        int randomGeneratedNumber = (int) (Math.random() * (100 - 1 + 1));
+        System.out.println("The number generated is "+ randomGeneratedNumber);
+
+        if (randomGeneratedNumber % 2 == 0){
+            System.out.println("This number is even! ");
+        }else {
+            System.out.println("This number is odd! ");
+        }
 
     }
 }
